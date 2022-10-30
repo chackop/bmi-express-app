e105d443f5c3a0e00123def27460e08518598e4b
Chacs <chacko@skykatltd.com>
2022-10-30 19:00:41 +0000

    test time zone

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import getLocationsWithTimezones from "./getLocationsWithTimezones";
 
 const app = express();
 const port = 3000;
+
+app.get("/timezones", getLocationsWithTimezones);
 
 app.listen(port, () => {
   console.log(`Application is running on port ${port}.`);
